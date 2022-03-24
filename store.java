9c304ff49fdf38be41cec36d2938f38ac485d488
MG-1410 <95872423+MG-1410@users.noreply.github.com>
2022-03-24 22:59:58 +0530

    Interface

---
+++ store.java
@@ -0,0 +1,8 @@
+package Billing;
+
+public interface store {
+
+    void add(int choice) throws Exception;
+    void delete(int choice) throws Exception;
+    void update(int choice) throws Exception;
+}
